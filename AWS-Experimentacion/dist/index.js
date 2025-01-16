ad0625bdfc84d74e7e3e964bce42833dd2f283fe
Maximo Sanchez <maximo.sanchez@darwoft.com>
2025-01-16 14:11:13 -0300

    initial commit

---
+++ AWS-Experimentacion/dist/index.js
@@ -0,0 +1,22 @@
+// Load the SDK for JavaScript
+import AWS from 'aws-sdk';
+// Set the Region 
+AWS.config.update({ region: 'us-east-2' });
+// Create S3 service object
+let s3 = new AWS.S3({ apiVersion: "2006-03-01" });
+// Call S3 to list the buckets
+s3.listBuckets(function (err, data) {
+    if (err) {
+        console.log("Error", err);
+    }
+    else {
+        console.log("Success", data.Buckets);
+    }
+});
+try {
+    let object = s3.getObject({ Bucket: 'maxys06-bucket-test', Key: 'bucket-file.txt' }).promise();
+    console.log(object);
+}
+catch (err) {
+    console.log(err);
+}
